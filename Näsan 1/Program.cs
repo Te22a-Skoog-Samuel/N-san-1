4704b695bafa59a3b9ad4f66045c348c1841d243
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2024-11-05 11:19:38 +0100

    Några uppgifter avklarade

---
+++ Näsan 1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+if (6 > 3)
+{
+
+    Console.WriteLine("Hello, World!");
+}
+
+string name = Console.ReadLine();
+string password = Console.ReadLine();
+
+if (name == "noname" && password == "nopass")
+{
+   
+   Console.WriteLine("Welcome!");
+   
+    Console.ReadLine();
+}
+
+else
+{
+
+Console.WriteLine("Wrong username or password");
+
+}
+
+int x = 0;
+
+while (x <= 32)
+{
+    x++;
+
+    Console.WriteLine("Hello, World!");
+}
