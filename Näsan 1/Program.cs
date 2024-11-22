2066e0dd2389b02b4c7d53cb300b56254b952753
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2024-11-22 12:27:14 +0100

    klar

--- Näsan 1/Program.cs
+++ Näsan 1/Program.cs
@@ -1,33 +1,80 @@
 ﻿
-if (6 > 3)
-{
 
-    Console.WriteLine("Hello, World!");
-}
+//  //if (6 > 3)
+// {
 
-string name = Console.ReadLine();
-string password = Console.ReadLine();
+//     Console.WriteLine("Hello, World!");
+// }
 
-if (name == "noname" && password == "nopass")
-{
+// //string name = Console.ReadLine();
+// //string password = Console.ReadLine();
+
+// //if (name == "kalleanka" && password == "12345")
+// {
    
-   Console.WriteLine("Welcome!");
+//    Console.WriteLine("Welcome!");
    
+//     Console.ReadLine();
+// }
+
+// //else
+// {
+
+// Console.WriteLine("Wrong username or password");
+
+// }
+
+// //int x = 0;
+
+// //while (x <= 32)
+// {
+//     x++;
+
+//     Console.WriteLine("Hello, World!");
+// }
+
+// //password = Console.ReadLine();
+
+// //while (password != "password")
+// {
+//     Console.WriteLine("Wrong!");
+//     password = Console.ReadLine();
+// }
+
+
+
+// //for (int y = 0; y<= 5; y++)
+// {
+
+// }
+
+while (true)
+{
+    string number = Console.ReadLine();
+    int result;
+    bool sanning = int.TryParse(number, out result);
+
+    if (result == 10)
+    {
+        Console.WriteLine("Mitt i prick!!");
+        break;
+    }
+    
+    if (result < 10 && sanning == true)
+    {
+        Console.WriteLine("Nej, gissa högre");
+    }
+
+    if (result > 10 && sanning == true)
+    {
+        Console.WriteLine("Nej, gissa lägre");
+    }
+
+    if (sanning == true)
+    {
+        Console.WriteLine("Du skrev inte ett nummer eller heltal");
+    }
+
     Console.ReadLine();
-}
 
-else
-{
-
-Console.WriteLine("Wrong username or password");
-
-}
-
-int x = 0;
-
-while (x <= 32)
-{
-    x++;
-
-    Console.WriteLine("Hello, World!");
 }
